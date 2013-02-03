89bd129fa5007a22c27694fa931160f6d440b842
Julien Quint <julien@igel.co.jp>
2013-02-03 11:32:25 +0900

    Using ByteString.Char8 for I/O

--- untrue.hs
+++ untrue.hs
@@ -1,12 +1,16 @@
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import qualified Data.List as L
+import Data.Word
+
 import Data.Bits
 import Data.Char
 import Data.Int
-import Data.List
 import Data.Maybe
 
 main = do
-  contents <- getContents
-  putStr $ map chr $ generate $ tokenize contents
+  contents <- B.getContents
+  B.putStr $ generate $ tokenize contents
 
 data Primitive = Ret | Add | Sub | Mul | Div | Neg | Eq | Gt | And | Or | Not |
                  Store | Fetch | Call | Dup | Drop | Swap | Rot | Pick | If |
@@ -29,28 +33,25 @@ opcodes = [(Ret, 0x80), (Add, 0x81), (Sub, 0x82), (Mul, 0x83), (Div, 0x84),
            (While, 0x94), (Puts, 0x95), (Puti, 0x96), (Putc, 0x97),
            (Getc, 0x98)]
 
-opcode :: Primitive -> Int
-opcode p = fromJust $ lookup p opcodes
+opcode :: Primitive -> Char
+opcode p = chr $ fromJust $ lookup p opcodes
 
-from_opcode :: Int -> Maybe Primitive
-from_opcode = flip lookup $ map (\x -> (snd x, fst x)) opcodes
-
-data Token = Function Primitive | LiteralString String | LiteralNumber Int |
-             Global Char | Lambda [Token] (Maybe Token)
+data Token = Function Primitive | LiteralString C.ByteString |
+             LiteralNumber Int | Global Char | Lambda [Token] (Maybe Token)
   deriving Show
 
 
-data TokenizerState = Chunk | StringToken String | NumberToken Int | Comment |
-     Quote
+data TokenizerState = Chunk | StringToken C.ByteString | NumberToken Int |
+                      Comment | Quote
 
 -- Tokenize the string into a Lambda token
-tokenize :: String -> Token
-tokenize xs =
+tokenize :: B.ByteString -> Token
+tokenize s =
   let globals = [LiteralNumber 0 | x <- ['a' .. 'z']]
       start = 6 + 4 * length globals
   in tokenize' Chunk
        (Lambda ([LiteralNumber start, Function Call, Function Ret] ++ globals)
-       Nothing) xs
+       Nothing) (C.unpack s)
 
 tokenize' :: TokenizerState -> Token -> String -> Token
 tokenize' Chunk token@(Lambda _ Nothing) "" = token
@@ -60,10 +61,10 @@ tokenize' Chunk (Lambda ts (Just (Lambda ts' p))) (']':xs) =
 tokenize' Chunk p ('{':xs) = tokenize' Comment p xs
 tokenize' Comment p ('}':xs) = tokenize' Chunk p xs
 tokenize' Comment p (_:xs) = tokenize' Comment p xs
-tokenize' Chunk p ('"':xs) = tokenize' (StringToken "") p xs
+tokenize' Chunk p ('"':xs) = tokenize' (StringToken C.empty) p xs
 tokenize' (StringToken s) (Lambda ts p) ('"':xs) =
   tokenize' Chunk (Lambda (ts ++ [LiteralString s]) p) xs
-tokenize' (StringToken s) t (x:xs) = tokenize' (StringToken (s ++ [x])) t xs
+tokenize' (StringToken s) t (x:xs) = tokenize' (StringToken (C.snoc s x)) t xs
 tokenize' Chunk t ('\'':xs) = tokenize' Quote t xs
 tokenize' Quote (Lambda ts p) (x:xs) =
   tokenize' Chunk (Lambda (ts ++ [LiteralNumber $ ord x]) p) xs
@@ -81,34 +82,34 @@ tokenize' (NumberToken n) (Lambda ts p) (x:xs)
       tokenize' (NumberToken $ 10 * n + (ord x - ord '0')) (Lambda ts p) xs
   | otherwise = tokenize' Chunk (Lambda (ts ++ [LiteralNumber n]) p) (x:xs)
 
-
 -- Generate code for the body of the top lambda expression
-generate :: Token -> [Int]
+generate :: Token -> C.ByteString
 generate (Lambda xs _) = generate' 0 0 xs
 
-generate' :: Int -> Int -> [Token] -> [Int]
+generate' :: Int -> Int -> [Token] -> C.ByteString
 generate' _ _ [] = generate'' (Function Ret)
 generate' m n ((Function op):xs) =
-  (generate'' $ Function op) ++ (generate' m (n + 1) xs)
+  C.append (generate'' $ Function op) (generate' m (n + 1) xs)
 generate' m n ((LiteralString s):xs) = let s' = generate'' $ LiteralString s in
-  s' ++ (generate' m (n + length s') xs)
+  C.append s' (generate' m (n + C.length s') xs)
 generate' m n ((LiteralNumber n'):xs) =
   let n'' = generate'' $ LiteralNumber n'
-  in n'' ++ (generate' m (n + length n'') xs)
+  in C.append n'' (generate' m (n + C.length n'') xs)
 generate' m n ((Global c):xs) =
   let c' = generate'' $ LiteralNumber $ 4 * (ord c - ord 'a') + 6
-  in c' ++ (generate' m (m + length c') xs)
+  in C.append c' (generate' m (m + C.length c') xs)
 generate' m n ((Lambda ys _):xs) =
   let xs' = generate' m (n + 4) xs
-      m' = m + n + 4 + length xs'
+      m' = m + n + 4 + C.length xs'
       ys' = generate' m' m' ys
-  in (generate'' $ LiteralNumber m') ++ xs' ++ ys'
+  in C.append (generate'' $ LiteralNumber m') $ C.append xs' ys'
 
-generate'' :: Token -> [Int]
-generate'' (Function op) = [opcode op]
+generate'' :: Token -> C.ByteString
+generate'' (Function op) = C.singleton $ opcode op
 generate'' (LiteralString s) =
-  (generate'' $ Function Puts) ++ (generate'' $ LiteralNumber $ length s) ++
-  (map ord s)
+  C.append (generate'' $ Function Puts) $
+    C.append (generate'' $ LiteralNumber $ C.length s) s
 generate'' (LiteralNumber n) =
-  (shift (n .&. 0x7f000000) (-24)) : (shift (n .&. 0xff0000) (-16)) :
-  (shift (n .&. 0xff00) (-8)) : (n .&. 0xff) : []
+  C.pack $ map chr $
+    (shift (n .&. 0x7f000000) (-24)) : (shift (n .&. 0xff0000) (-16)) :
+    (shift (n .&. 0xff00) (-8)) : (n .&. 0xff) : []
